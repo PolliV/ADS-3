ba0efe14bb4389875bd5ec9aa236e726231de504
PolliV <77505747+PolliV@users.noreply.github.com>
2023-03-20 16:49:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int roof = 0;
+  int floor = size - 1;
+  int mide;
+  int count = 0;
+  while (roof <= floor)
+  {
+    mide = (floor + roof) / 2;
+    count++;
+    if (value < arr[mide]) {
+      floor = mide - 1;
+    } else if (value > arr[mide]) {
+      roof = mide + 1;
+    } else {
+      for (int i = 0; arr[mide] == arr[mide - 1]; i++) {
+        count++;
+      }
+      for (int i = 0; arr[mide] == arr[mide + 1]; i++) {
+        count++;
+      }
+      return count;
+    }
+  }
+  return 0;
 }
